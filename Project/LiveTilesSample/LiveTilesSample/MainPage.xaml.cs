2c4832d904feeacefbba8536714a4acaa7e7bb33
unknown <joao.cunha@DSES056.corp.eldorado.org.br>
2015-09-18 15:49:38 -0300

    Added BackgroundTask project. It is not added into the solution by default. Also included code to register the task. It is commented by default.

--- Project/LiveTilesSample/LiveTilesSample/MainPage.xaml.cs
+++ Project/LiveTilesSample/LiveTilesSample/MainPage.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Background;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -27,9 +29,57 @@ namespace LiveTilesSample
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void SetTileButton_Click(object sender, RoutedEventArgs e)
         {
             TileManager.UpdateLiveTile(this.liveTileContentText.Text);
         }
+                
+        //private void RegisterTaskButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    this.RegisterTask();
+        //}
+
+        ///// <summary>
+        ///// Register the task for the application
+        ///// </summary>
+        //private async void RegisterTask()
+        //{
+        //    var taskRegistered = Windows.ApplicationModel.Background.BackgroundTaskRegistration.AllTasks.Select(task => task.Value.Name == nameof(TileUpdaterTask)).Count() > 0;
+        //    if (taskRegistered)
+        //    {
+        //        this.ShowDialog("Tarefa já registrada!");
+        //    }
+        //    else
+        //    {
+        //        // Check if task is registred
+        //        BackgroundAccessStatus allowed = await BackgroundExecutionManager.RequestAccessAsync();
+
+        //        if (allowed == BackgroundAccessStatus.AllowedMayUseActiveRealTimeConnectivity || allowed == BackgroundAccessStatus.AllowedWithAlwaysOnRealTimeConnectivity)
+        //        {
+        //            var task = new BackgroundTaskBuilder
+        //            {
+        //                Name = nameof(TileUpdaterTask),
+        //                CancelOnConditionLoss = false,
+        //                TaskEntryPoint = typeof(TileUpdaterTask).ToString(),
+        //            };
+
+        //            task.SetTrigger(new TimeTrigger(15, false));
+        //            task.AddCondition(new SystemCondition(SystemConditionType.UserPresent));
+        //            task.Register();
+
+        //            this.ShowDialog("Tarefa registrada com sucesso!");
+        //        }
+        //        else
+        //        {
+        //            this.ShowDialog("Não foi possível registrar a tarefa!");                    
+        //        }
+        //    }
+        //}
+
+        //private async void ShowDialog(string content)
+        //{
+        //    MessageDialog dialog = new MessageDialog(content);
+        //    await dialog.ShowAsync();
+        //}
     }
 }
