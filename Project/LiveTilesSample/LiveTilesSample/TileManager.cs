01faa2ed5b1b1c6639707ee6978d96fa9a364aa1
Diego Castro <diego.castro@eldorado.org.br>
2015-09-21 09:33:19 -0300

    Added images

--- Project/LiveTilesSample/LiveTilesSample/TileManager.cs
+++ Project/LiveTilesSample/LiveTilesSample/TileManager.cs
@@ -13,64 +13,67 @@ namespace LiveTilesSample
         public static void UpdateLiveTile(string content)
         {
 
-            string tileXml = @"<tile>
-                                <visual>
-                                 <binding template=""TileSmall"">
-                                  <text>Small</text>
-                                 </binding>
-                                 <binding template=""TileMedium"">
-                                  <text>Medium</text>
-                                 </binding>
-                                 <binding template=""TileWide"">
-                                  <text>Wide</text>
-                                 </binding>
-                                 <binding template=""TileLarge"">
-                                  <text>Large</text>
-                                 </binding>
-                                </visual>
-                               </tile>";
-
             //string tileXml = @"<tile>
             //                    <visual>
             //                     <binding template=""TileSmall"">
             //                      <text>Small</text>
             //                     </binding>
-            //                     <binding template=""TileMedium"" branding=""name"">
-            //                      <image placement=""peek"" src=""Assets\Tile310x150.png"" />
-            //                      <text hint-wrap=""true"" hint-maxLines=""3"">Trilha Universal Windows</text>
+            //                     <binding template=""TileMedium"">
+            //                      <text>Medium</text>
             //                     </binding>
-            //                     <binding template=""TileWide"">                                  
-            //                      <group>
-            //                       <subgroup hint-weight=""33"">
-            //                        <image placement=""inline"" src=""Assets\TDCLogo.png"" />
-            //                       </subgroup>
-            //                       <subgroup>
-            //                        <text hint-style=""caption"" hint-wrap=""true"" hint-maxLines=""3"">
-            //                          Trilha Universal Windows
-            //                        </text>
-            //                        <text hint-style=""captionsubtle"" hint-wrap=""true"" hint-maxLines=""3"">
-            //                          Criando Adaptive Tiles
-            //                        </text>
-            //                       </subgroup>
-            //                      </group>
+            //                     <binding template=""TileWide"">
+            //                      <text>Wide</text>
             //                     </binding>
-            //                     <binding template=""TileLarge"" hint-textStacking=""center"" branding=""name"">
-            //                      <group>
-            //                       <subgroup hint-weight=""1"" />
-            //                       <subgroup hint-weight=""2"">
-            //                        <image src=""Assets\Tile150x150.png"" hint-crop=""circle"" />
-            //                       </subgroup>
-            //                       <subgroup hint-weight=""1"" />
-            //                      </group>
-            //                      <text hint-style=""title"" hint-wrap=""true"" hint-maxLines=""3"" hint-align=""center"">
-            //                        Universal Windows
-            //                      </text>
-            //                      <text hint-style=""caption"" hint-wrap=""true"" hint-maxLines=""3"" hint-align=""center"">
-            //                        Criando Live Tiles
-            //                      </text>
+            //                     <binding template=""TileLarge"">
+            //                      <text>Large</text>
             //                     </binding>
             //                    </visual>
             //                   </tile>";
+
+            string tileXml = string.Format(@"<tile>
+                                <visual>
+                                 <binding template=""TileSmall"">
+                                  <text>Small</text>
+                                 </binding>
+
+                                 <binding template=""TileMedium"" branding=""name"">
+                                  <image placement=""peek"" src=""Assets\Tile150x150.png"" />
+                                  <text hint-wrap=""true"" hint-maxLines=""3"">{0}</text>
+                                 </binding>
+
+                                 <binding template=""TileWide"">                                  
+                                  <group>
+                                   <subgroup hint-weight=""33"">
+                                    <image placement=""inline"" src=""Assets\Tile150x150Transparent.png"" />
+                                   </subgroup>
+                                   <subgroup>
+                                    <text hint-style=""caption"" hint-wrap=""true"" hint-maxLines=""3"">
+                                      MVA Live Tiles
+                                    </text>
+                                    <text hint-style=""captionsubtle"" hint-wrap=""true"" hint-maxLines=""3"">
+                                      {1}
+                                    </text>
+                                   </subgroup>
+                                  </group>
+                                 </binding>
+
+                                 <binding template=""TileLarge"" hint-textStacking=""center"" branding=""name"">
+                                  <group>
+                                   <subgroup hint-weight=""1"" />
+                                   <subgroup hint-weight=""2"">
+                                    <image src=""Assets\Tile150x150White.png"" hint-crop=""circle"" />
+                                   </subgroup>
+                                   <subgroup hint-weight=""1"" />
+                                  </group>
+                                  <text hint-style=""title"" hint-wrap=""true"" hint-maxLines=""3"" hint-align=""center"">
+                                    MVA Live Tiles
+                                  </text>
+                                  <text hint-style=""caption"" hint-wrap=""true"" hint-maxLines=""3"" hint-align=""center"">
+                                    {2}
+                                  </text>
+                                 </binding>
+                                </visual>
+                               </tile>", content, content, content);
 
             XmlDocument xmldoc = new XmlDocument();
             xmldoc.LoadXml(tileXml);
@@ -78,7 +81,7 @@ namespace LiveTilesSample
             TileNotification tileNotification = new TileNotification(xmldoc);
             TileUpdater tileUpdator = TileUpdateManager.CreateTileUpdaterForApplication();
 
-            tileUpdator.Update(tileNotification);
+            tileUpdator.Update(tileNotification);           
         }
     }
 }
